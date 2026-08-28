b049bd74a1e1c94a62c360584327c26fd098e161
Terranom674 <thomas_dannenberg@web.de>
2026-08-28 20:51:21 +0200

    fix(i18n): make partial German locale loadable

--- client/src/config/app/locale/de-DE/de-DE.js
+++ client/src/config/app/locale/de-DE/de-DE.js
@@ -1,13 +1,11 @@
 /* eslint-disable import/extensions */
 import base from "./json/base.json";
-import reader from "./json/reader";
 import shared from "./json/shared";
 import de from "date-fns/locale/de";
 
 export default {
   translation: {
     ...base,
-    ...reader,
     ...shared,
     date_fns: de
   }
