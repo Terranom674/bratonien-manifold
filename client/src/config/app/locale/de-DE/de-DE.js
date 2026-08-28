5d5518c9405154c36a8eb76b13f69c9b7fb3c51c
Terranom674 <thomas_dannenberg@web.de>
2026-08-28 20:50:10 +0200

    refactor(i18n): use partial German locale with English fallback

--- client/src/config/app/locale/de-DE/de-DE.js
+++ client/src/config/app/locale/de-DE/de-DE.js
@@ -1,16 +1,12 @@
 /* eslint-disable import/extensions */
 import base from "./json/base.json";
-import backend from "./json/backend";
 import reader from "./json/reader";
-import frontend from "./json/frontend";
 import shared from "./json/shared";
 import de from "date-fns/locale/de";
 
 export default {
   translation: {
     ...base,
-    ...backend,
-    ...frontend,
     ...reader,
     ...shared,
     date_fns: de
