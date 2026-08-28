420ec52e4cfe6706bb170f281aa5b455b4cae905
Terranom674 <thomas_dannenberg@web.de>
2026-08-28 21:03:24 +0200

    feat(i18n): enable German backend translations

--- client/src/config/app/locale/de-DE/de-DE.js
+++ client/src/config/app/locale/de-DE/de-DE.js
@@ -1,13 +1,15 @@
 /* eslint-disable import/extensions */
 import base from "./json/base.json";
+import backend from "./json/backend";
 import frontend from "./json/frontend";
 import reader from "./json/reader";
 import shared from "./json/shared";
-import de from "date-fns/locale/de";
+import { de } from "date-fns/locale/de";
 
 export default {
   translation: {
     ...base,
+    ...backend,
     ...frontend,
     ...reader,
     ...shared,
