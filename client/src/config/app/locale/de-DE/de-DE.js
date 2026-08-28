9fe82320d14fa050a953fec8eedb3eb6149b92d8
Terranom674 <thomas_dannenberg@web.de>
2026-08-28 20:58:43 +0200

    feat(i18n): enable German frontend and reader translations

--- client/src/config/app/locale/de-DE/de-DE.js
+++ client/src/config/app/locale/de-DE/de-DE.js
@@ -1,11 +1,15 @@
 /* eslint-disable import/extensions */
 import base from "./json/base.json";
+import frontend from "./json/frontend";
+import reader from "./json/reader";
 import shared from "./json/shared";
 import de from "date-fns/locale/de";
 
 export default {
   translation: {
     ...base,
+    ...frontend,
+    ...reader,
     ...shared,
     date_fns: de
   }
