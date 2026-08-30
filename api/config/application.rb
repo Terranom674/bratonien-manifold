990bae50a8087f5213fb22f883503d6f769a8b31
Terranom674 <thomas_dannenberg@web.de>
2026-08-30 22:29:53 +0200

    Set Rails default locale to German

--- api/config/application.rb
+++ api/config/application.rb
@@ -74,10 +74,10 @@ module ManifoldApi
     # Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from
+    # The default locale is German and all translations from
     # config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Rails.root.glob('config/locales/**/*.{rb,yml}')
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :de
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
