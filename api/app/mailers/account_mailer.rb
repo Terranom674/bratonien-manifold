187f5b9443befc569008309b85ac724e1e297db1
Terranom674 <thomas_dannenberg@web.de>
2026-08-30 21:52:12 +0200

    Translate Manifold account emails to German

--- api/app/mailers/account_mailer.rb
+++ api/app/mailers/account_mailer.rb
@@ -4,23 +4,23 @@ class AccountMailer < ApplicationMailer
   def email_confirmation(user)
     @email_confirmation_url = @api_routes.email_confirmation_url_for(user)
     @user = user
-    mail(to: @user.email, subject: "Confirm Your Email")
+    mail(to: @user.email, subject: "E-Mail-Adresse bestätigen")
   end
 
   def reset_password(user)
     @user = user
-    mail(to: @user.email, subject: "Password reset requested")
+    mail(to: @user.email, subject: "Zurücksetzen des Passworts angefordert")
   end
 
   def password_change_notification(user)
     @user = user
-    mail(to: @user.email, subject: "Your password has been reset")
+    mail(to: @user.email, subject: "Dein Passwort wurde zurückgesetzt")
   end
 
   def welcome(user, created_by_admin: false)
     @email_confirmation_url = @api_routes.email_confirmation_url_for(user)
     @user = user
     @created_by_admin = created_by_admin
-    mail(to: @user.email, subject: "Welcome to Manifold!")
+    mail(to: @user.email, subject: "Willkommen bei Manifold!")
   end
 end
